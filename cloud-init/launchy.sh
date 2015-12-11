36d0845b96936fecb9aad8ae359792f250ecc8df
Dave Liggat <dave@liggat.org>
2015-12-10 19:28:35 -0800

    Add options for both ubuntu and amazon linux

--- cloud-init/launchy.sh
+++ cloud-init/launchy.sh
@@ -1,12 +1,23 @@
 #/bin/bash
 
-AMI=ami-d5c5d1e5                         # Amazon linux.
+
+if [[ "${UBUNTU:-false}" = "true" ]]; then
+  echo "Using Ubuntu"
+  AMI=ami-5189a661  # Ubuntu.
+  SSHUSER=ubuntu
+  USER_DATA="file://ubuntu.yml"
+else
+  echo "Using Amazon Linux"
+  AMI=ami-d5c5d1e5  # Amazon linux.
+  SSHUSER=ec2-user
+  USER_DATA="file://amazon-linux.yml"
+fi
+
 SECURITY_GROUP_NAME="dliggat-web-sg"     # HTTP/HTTPS/SSH ingress.
 KEY_NAME=daveliggat-trinimbus
 KEY_PATH="${HOME}/.ssh/${KEY_NAME}.pem"
 
-USER_DATA="file://user-data.yml"
-EC2_TAG="${EC2_TAG:-dliggat-load-balancing-exp}"
+EC2_TAG="${EC2_TAG:-dliggat}"
 INSTANCE_COUNT="${INSTANCE_COUNT:-1}"
 SLEEP_TIME=15
 INSTANCE_TYPE=m3.medium
@@ -28,7 +39,7 @@ aws ec2 create-tags --resources ${INSTANCE} --tags Key=Name,Value=${EC2_TAG}
 
 PUBLIC_IP=$(aws ec2 describe-instances --instance-ids ${INSTANCE} | jq -r '.Reservations[0].Instances[0].PublicIpAddress')
 echo ${PUBLIC_IP}
-echo "ssh -i ${KEY_PATH} ec2-user@${PUBLIC_IP}"
+echo "ssh -i ${KEY_PATH} ${SSHUSER}@${PUBLIC_IP}"
 
 # eval "function connect { ssh -v -i ${KEY_PATH} root@${PUBLIC_IP} }"
 # eval "function delete { aws ec2 stop-instances --instance-ids ${INSTANCE} }"
